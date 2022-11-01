41a9d8436889b0c65e94d084fb1001ddd842c9c6
LuisCH10 <chicomaluis555@gmail.com>
2022-11-01 11:48:44 -0500

    enviando todas las carpetas de clases y el archivo .gitignore

---
+++ EscrbiendoHtmlDesdeJS/script.js
@@ -0,0 +1,40 @@
+// h1 {color: red}
+// .parrafito { ... }
+// #pid {...}
+
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+});
+
+h1.innerHTML = 'PATITO';
+
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class','rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+input.value = "456"
+
+const img = document.createElement('img');
+
+img.setAttribute('src','./Captura desde 2022-09-17 15-53-03.png');
+
+console.log(img);
+
+pid.innerHTML = "";
+pid.append(img);
